a1820cb3c486b54df751d988ec00a392fd58230a
Annie Tang <annietang@berkeley.edu>
2018-08-12 00:05:34 -0700

    reverted main.js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$("input").click(function() {
-    if ($("input:checked").length != 0) {
-        $('input').prop('checked', true);
-    } else {
-        $('input').prop('checked', false);
-    }
-});
